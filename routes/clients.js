636e5169a8f636fea9490477c46039f00c4d8b45
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-04-22 14:07:10 -0500

    Client routes and position CREATE and READ

--- routes/clients.js
+++ routes/clients.js
@@ -1,8 +1,13 @@
 var express = require('express');
-const { getClients } = require('../controllers/clientController');
+const { getClients, createPosition, getPositions, getPositionById } = require('../controllers/clientController');
 const router = express.Router();
 
 /* GET clients */
 router.get('/', getClients);
+
+/* Create Position */ 
+router.get('/positions', getPositions);
+router.get('/positions/:id', getPositionById);
+router.post('/positions', createPosition);
 
 module.exports = router;
