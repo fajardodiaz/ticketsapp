8339bbd5b1f187b6238cd3240455bf7ce51a8558
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-04-27 12:03:04 -0500

    Adding the controllers for table area

---
+++ controllers/client/areaController.js
@@ -0,0 +1,103 @@
+const {
+    Area
+} = require("../../models/clientModel");
+
+
+// Area Operations
+
+//Get All Areas
+exports.getAreas = async (req, res) => {
+    try {
+        const areas = await Area.findAll();
+        res.json(areas);
+    } catch (error) {
+        res.json({
+            "message": error
+        });
+    }
+}
+
+//Get an Area By ID
+exports.getAreaById = async (req, res) => {
+    try {
+        const area = await Area.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json(position);
+
+    } catch (error) {
+        res.json({
+            "message": error
+        });
+    }
+}
+
+//Create One/Many Areas
+exports.createArea = async (req, res) => {
+    try {
+        let data = req.body;
+
+        //If data.length > 1 means that we send an array to create many areas in a single post request
+        if (data.length > 1) {
+            await Area.bulkCreate(data);
+            res.json({
+                "message": "Areas created succesfully"
+            })
+        } else {
+            await Area.create({
+                name: req.body.name
+            });
+            res.json({
+                "message": "Area Created Succesfully"
+            });
+        }
+
+    } catch (error) {
+        res.json({
+            "message": error
+        });
+    }
+}
+
+
+//Update an Area
+exports.updateArea = async (req, res) => {
+    try {
+        const updateArea = await Area.update({
+            name: req.body.name
+        }, {
+            where: {
+                id: req.params.id
+            }
+        });
+
+        res.json({
+            "message": "Area updated Succesfully"
+        });
+    } catch (error) {
+        console.log(error)
+        res.json({
+            "error": error
+        })
+    }
+}
+
+//Delete an Position
+exports.deleteArea = async (req, res) => {
+    try {
+        const deleteArea = await Area.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.json({
+            "message": "Area deleted Succesfully"
+        });
+    } catch (error) {
+        res.json({
+            "error": error
+        })
+    }
+}
