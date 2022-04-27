af2f6a87b1b09a74b63c847793c239ba340a970f
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-04-27 05:28:33 -0500

    update the controllers in the routes

--- routes/clients.js
+++ routes/clients.js
@@ -1,13 +1,16 @@
 var express = require('express');
-const { getClients, createPosition, getPositions, getPositionById } = require('../controllers/clientController');
+const {createPosition, getPositions, getPositionById, updatePosition, deletePosition, createManyPositions } = require('../controllers/client/positionController')
 const router = express.Router();
 
 /* GET clients */
-router.get('/', getClients);
+// router.get('/', getClients);
 
-/* Create Position */ 
+/* Positions */ 
 router.get('/positions', getPositions);
 router.get('/positions/:id', getPositionById);
 router.post('/positions', createPosition);
+router.post('/positions/many', createManyPositions);
+router.put('/positions/:id', updatePosition);
+router.delete('/positions/:id', deletePosition);
 
 module.exports = router;
